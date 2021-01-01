04e2799848682656acbc765955f3565fbaf9d4b4
Johannes Schultz <git42@sagagames.de>
2021-01-01 21:22:17 +0100

    Update SQL schema to use utf8mb4

--- schema.sql
+++ schema.sql
@@ -1,55 +1,77 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 
-CREATE TABLE IF NOT EXISTS `compos` (
-  `idcompo` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `active` tinyint(4) unsigned NOT NULL DEFAULT '1',
-  `idhost` int(10) unsigned NOT NULL,
-  `start_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `downloadable` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`idcompo`),
-  KEY `idhost` (`idhost`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+CREATE TABLE `compos` (
+  `idcompo` int(10) UNSIGNED NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` tinyint(4) UNSIGNED NOT NULL DEFAULT 1,
+  `idhost` int(10) UNSIGNED NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `downloadable` tinyint(3) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `entries` (
-  `identry` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `author` varchar(64) NOT NULL,
-  `filename` varchar(64) NOT NULL,
-  `title` varchar(32) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `idcompo` int(10) unsigned NOT NULL,
-  `altered` tinyint(3) unsigned NOT NULL DEFAULT '0',
+CREATE TABLE `entries` (
+  `identry` int(10) UNSIGNED NOT NULL,
+  `author` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `filename` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `idcompo` int(10) UNSIGNED NOT NULL,
+  `altered` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `points` int(11) DEFAULT NULL,
-  `place` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`identry`),
-  KEY `idcompo` (`idcompo`),
-  KEY `points` (`points`),
-  KEY `place` (`place`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+  `place` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `hosts` (
-  `idhost` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `hostname` varchar(64) NOT NULL,
+CREATE TABLE `hosts` (
+  `idhost` int(10) UNSIGNED NOT NULL,
+  `hostname` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(328) NOT NULL,
-  `access_level` tinyint(4) unsigned NOT NULL,
-  PRIMARY KEY (`idhost`),
-  UNIQUE KEY `hostname_UNIQUE` (`hostname`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+  `access_level` tinyint(4) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `uploading` (
-  `idupload` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `uploading` (
+  `idupload` int(10) UNSIGNED NOT NULL,
   `author` varchar(64) NOT NULL,
-  `start` int(10) unsigned NOT NULL,
-  `idcompo` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idupload`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+  `start` int(10) UNSIGNED NOT NULL,
+  `idcompo` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `compos`
+  ADD PRIMARY KEY (`idcompo`),
+  ADD KEY `idhost` (`idhost`);
+
+ALTER TABLE `entries`
+  ADD PRIMARY KEY (`identry`),
+  ADD KEY `idcompo` (`idcompo`),
+  ADD KEY `points` (`points`),
+  ADD KEY `place` (`place`);
+
+ALTER TABLE `hosts`
+  ADD PRIMARY KEY (`idhost`),
+  ADD UNIQUE KEY `hostname_UNIQUE` (`hostname`);
+
+ALTER TABLE `uploading`
+  ADD PRIMARY KEY (`idupload`);
+
+
+ALTER TABLE `compos`
+  MODIFY `idcompo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `entries`
+  MODIFY `identry` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `hosts`
+  MODIFY `idhost` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `uploading`
+  MODIFY `idupload` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
